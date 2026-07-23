9e866f734672586a04ea137c32ac4e750f3ea153
Devin Dennis <93836398+Raccoonee@users.noreply.github.com>
2026-07-23 12:52:15 -0500

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = []
+        for i in range(len(temperatures)):
+            temp = temperatures.pop(0)
+            day = 0
+
+            for j in range(len(temperatures)):
+                day += 1
+                if temp < temperatures[j]:
+                    break
+            else:
+                day = 0
+                
+            res.append(day)
+
+        return res
+        
